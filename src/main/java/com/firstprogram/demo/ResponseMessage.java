340182a8f238e7ce5f337fbc27c98e737a4b96a4
EZ <dza9393@mail.ru>
2023-04-29 13:23:29 +0300

    Post refined

---
+++ src/main/java/com/firstprogram/demo/ResponseMessage.java
@@ -0,0 +1,45 @@
+package com.firstprogram.demo;
+
+public class ResponseMessage {
+
+    public long id;
+    public String author;
+    public AppError appError;
+
+    public ResponseMessage(long id, String author, AppError appError) {
+        this.id = id;
+        this.author = author;
+        this.appError = appError;
+    }
+
+    public ResponseMessage(long id, String author) {
+        this.id = id;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public AppError getAppError() {
+        return appError;
+    }
+
+    public void setAppError(AppError appError) {
+        this.appError = appError;
+    }
+    
+    
+}
